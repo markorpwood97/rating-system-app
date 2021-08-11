3e6a79a443772a1d24eb357a1bc7283a204f5627
Mark <58310560+markorpwood97@users.noreply.github.com>
2021-08-11 22:19:51 +0200

    Added star rating and disable button functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from "@angular/common/http";
+import { Rating } from './rating.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Injectable()
+export class AppComponent implements OnInit {
   title = 'rating-system-app';
+  feedbackForm: FormGroup;
+  error = new Subject<string>();
+  currentRate = 0;   
+
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit() {
+    this.initForm();
+  }
+  
+  onSubmit() {
+    this.saveRating(this.feedbackForm.value['rating'], this.feedbackForm.value['reason']);
+  };
+  
+  private saveRating(rating: number, reason: string) {
+    const ratingItem: Rating = {rating: rating, reason: reason};
+    this.http.post('https://rating-system-c8396-default-rtdb.firebaseio.com/posts.json', 
+      ratingItem,
+      {
+        observe: 'response'
+      }
+    )
+    .subscribe(responseData => {
+      console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
+    });
+  }
+
+  private initForm() {
+    this.feedbackForm = new FormGroup({
+      rating: new FormControl(0, Validators.min(1)),
+      reason: new FormControl()
+    });
+  }
 }
---
+++ src/app/rating.model.ts
@@ -0,0 +1,9 @@
+export class Rating {
+    public rating: number;
+    public reason: string;
+
+    constructor(rating: number, reason: string) {
+        this.rating = rating;
+        this.reason = reason;
+    }
+}
